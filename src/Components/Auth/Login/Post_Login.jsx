e8f988966f26974b105b2c5a5dd76ad3f6e04e85
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-14 00:58:17 +0100

    init auth handlers

---
+++ src/Components/Auth/Login/Post_Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Post_Login() {
+  return (
+    <div>Post_Login</div>
+  )
+}
+
+export default Post_Login
---
+++ src/Components/Auth/Register/Post_Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Post_Register() {
+  return (
+    <div>Post_Register</div>
+  )
+}
+
+export default Post_Register
