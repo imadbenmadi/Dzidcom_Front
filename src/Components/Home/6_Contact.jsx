77512fb63b005473e96f3a76933a78cea98f351d
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-15 15:01:40 +0100

    chore: Add tailwind-scrollbar plugin and update colors in tailwind.config.js

--- src/Components/Home/6_Contact.jsx
+++ src/Components/Home/6_Contact.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function Contact() {
-    return <div>Contact</div>;
+    return (
+        <div className=" flex justify-center items-center py-6 gap-8">
+            <div>
+                <div>Connect</div>
+                <div>Contact Support</div>
+                <div>
+                    Have a question? need assistance? get in touch with our
+                    support team
+                </div>
+                <div>
+                    <div className=" flex justify-center items-center gap-8 ">
+                        <input type="text" className=" bo"/>
+                        <input type="text" />
+                    </div>
+                </div>
+                <div>
+                    <input type="text" />
+                </div>
+                <div>
+                    <textarea name="" id=""></textarea>
+                </div>
+            </div>
+            <div className=" hidden md:block"></div>
+        </div>
+    );
 }
 
 export default Contact;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                red_error: "#F84141",
+                green: "#469F78",
+                yallow: "#F6BC63",
+                perpol: "#9DA0E3",
+                black_text: "#1E1E1E",
+                gray: "#454545",
+                gray_white: "#e6e6e6",
+                image_animation: "#b6b2b22e",
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        require("tailwind-scrollbar")({
+            nocompatible: true,
+            preferredStrategy: "pseudoelements",
+        }),
+    ],
 };
