adfe41d8e4676c61e3322f4588296f46a18d394d
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-20 22:39:50 +0100

    adding logout to navbar laptop

--- src/Components/Freelancer/NavBar/Laptop_Nav_Items.jsx
+++ src/Components/Freelancer/NavBar/Laptop_Nav_Items.jsx
@@ -3,7 +3,20 @@ import Logo from "../../../../public/Logo.png";
 import message_icon from "../../../../public/Profile/message.png";
 import notification_icon from "../../../../public/Profile/Notification.png";
 import user_image from "../../../../public/user2.png";
-function Laptop_Nav_Items({ isProfileCompleted, Active_nav }) {
+import { useState } from "react";
+import { TbLogout2 } from "react-icons/tb";
+import { CiUser } from "react-icons/ci";
+
+function Laptop_Nav_Items({
+    isProfileCompleted,
+    Active_nav,
+    handleLogout,
+    LogoutClicked,
+}) {
+    const [ProfileClicked, setProfileClicked] = useState(false);
+    const toogleProfile = () => {
+        setProfileClicked(!ProfileClicked);
+    };
     return (
         <div className="hidden  md:flex  items-center justify-between mx-2 lg:mx-12  md:text-md lg:text-lg  font-[500] text-black_text h-full p-2 ">
             <div>
@@ -79,13 +92,57 @@ function Laptop_Nav_Items({ isProfileCompleted, Active_nav }) {
                     <div>
                         <img src={notification_icon} alt="" />
                     </div>
-                    <div>
-                        <img src={user_image} alt="" className=" w-8" />
-                    </div>
                 </div>
             ) : (
-                <div className=" w-[4.5rem] lg:w-[7.9rem]"></div>
+                <div></div>
             )}
+            <div className=" relative">
+                <img
+                    src={user_image}
+                    alt=""
+                    className=" w-8 cursor-pointer"
+                    onClick={toogleProfile}
+                />
+                {ProfileClicked ? (
+                    <div
+                        className="absolute top-10 right-0 bg-white shadow border  
+                    rounded-lg p-2 w-40 z-50 flex items-center  flex-col gap-3"
+                    >
+                        <div
+                            className="text-black_text cursor-pointer w-[80px] "
+                            onClick={() => {
+                                setProfileClicked(false);
+                            }}
+                        >
+                            <Link
+                                to={"/Freelancer/Profile"}
+                                className=" select-none flex items-center gap-2 "
+                            >
+                                <CiUser className=" shrink-0 text-xl " />
+                                Profil
+                            </Link>
+                        </div>
+                        <div className="">
+                            {LogoutClicked ? (
+                                <div className="w-full ">
+                                    <span className="small-loader font-bold  w-full m-auto"></span>
+                                </div>
+                            ) : (
+                                <div
+                                    className="cursor-pointer w-full 
+                                    flex items-center gap-3 text-red-500"
+                                    onClick={() => {
+                                        handleLogout();
+                                    }}
+                                >
+                                    <TbLogout2 className=" shrink-0 text-xl" />
+                                    Logout
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                ) : null}
+            </div>{" "}
         </div>
     );
 }
