ff6c3aee21b564df51033e44470558b64430735d
imadbenmadi <imadbenmadi891@gmail.com>
2024-05-20 22:14:34 +0100

    .

--- src/Default.jsx
+++ src/Default.jsx
@@ -8,15 +8,14 @@ function Default() {
     console.log("data from default", isAuth, userType);
     const Navigate = useNavigate();
     useEffect(() => {
-         throw new Error("error from default page");
-        // if (!isAuth || !userType) Navigate("/Home");
-        // else if (isAuth && userType == "client") {
-        //     // Navigate(`/Client/${userId}`);
-        //     Navigate(`/Client`);
-        // } else if (isAuth && userType == "freelancer") {
-        //     // Navigate(`/Freelancer/${userId}`);
-        //     Navigate(`/Freelancer`);
-        // } else Navigate("/Home");
+        if (!isAuth || !userType) Navigate("/Home");
+        else if (isAuth && userType == "client") {
+            // Navigate(`/Client/${userId}`);
+            Navigate(`/Client`);
+        } else if (isAuth && userType == "freelancer") {
+            // Navigate(`/Freelancer/${userId}`);
+            Navigate(`/Freelancer`);
+        } else Navigate("/Home");
     }, []);
 }
 export default Default;
