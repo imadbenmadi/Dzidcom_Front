76e046b8e31f72732b1703bb30ba8009cabcb40f
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-05-25 02:23:42 +0100

    Improve responsiveness and fix navigation bugs in Home component

--- src/Components/Home/Hero/HeroRight.jsx
+++ src/Components/Home/Hero/HeroRight.jsx
@@ -6,7 +6,7 @@ import iconHome from "../../../../public/Home/Hero/icon.png";
 function HeroRight() {
   return (
     <div className="w-full mt-10">
-      <div className="flex gap-2 items-center relative w-full mt-2">
+      <div className="flex gap-2 float-right items-center relative w-[90%] mb-4">
         <img
           src={iconHome}
           className="lg:w-16  max-md:w-12  h-fit  z-10 md:w-12 max-md:-left-10   -left-12 -bottom-5 self-end absolute"
@@ -15,18 +15,18 @@ function HeroRight() {
         <img
           src={Hero1}
           alt=""
-          className=" md:h-36  max-md:w-56 mr-0 max-md:h-24 z-20 h-48 w-full "
+          className=" md:h-36   max-md:w-56 mr-0 max-md:h-20 z-20 h-46 w-full "
         />
       </div>
-      <div className="flex gap-5 w-full  mt-5">
+      <div className="flex justify-between  items-center gap-5 w-full  mt-5">
         <img
           src={Hero2}
-          className="lg:h-40 lg:w-[50%] md:w-[50%] max-md:h-28  max-md:w-28 "
+          className="lg:h-40   lg:w-[50%] md:w-[50%] max-md:h-28  max-md:w-28 "
           alt=""
         />
         <img
           src={Hero3}
-          className="h-56 md:w-[50%] md:h-36  max-md:h-28 max-md:w-28"
+          className="h-56 md:w-[50%] md:h-42  max-md:h-28 max-md:w-28"
           alt=""
         />
       </div>
