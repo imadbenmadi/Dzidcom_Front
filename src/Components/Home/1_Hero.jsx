22e028af91aa2039031646526ed522c62b788aaa
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-05-25 14:39:43 +0100

    Update Home component layout and image loading add framer motion

--- src/Components/Home/1_Hero.jsx
+++ src/Components/Home/1_Hero.jsx
@@ -1,17 +1,41 @@
-import React from "react";
-import HeroLeft from "./Hero/HeroLeft";
-import HeroRight from "./Hero/HeroRight";
+import { motion } from "framer-motion";
+import HeroLeft from "./Hero/HeroLeft"; // Import your HeroLeft component
+import HeroRight from "./Hero/HeroRight"; // Import your HeroRight component
+
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5, // delay between child animations
+    },
+  },
+};
+
+const itemVariantsLeft = {
+  hidden: { opacity: 0, x: 50 },
+  visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+};
+const itemVariantsRight = {
+  hidden: { opacity: 0, x: -50 },
+  visible: { opacity: 1, x: 0, transition: { duration: 1 } },
+};
 
 function Hero() {
   return (
-    <div className="flex lg:min-h-fit   min-h-screen justify-between w-full max-md:flex-col mt-10 h-full items-center mx-auto max-w-[1200px]">
-      <div className="md:w-[50%] lg:w-[55%] h-fit">
-        <HeroLeft />
-      </div>
-      <div className="md:w-[45%]   lg:w-[40%] p-5 h-fit">
-        <HeroRight />
-      </div>
-    </div>
+    <motion.div
+      className="flex lg:min-h-fit min-h-screen justify-between w-full max-md:flex-col mt-10 h-full items-center mx-auto max-w-[1200px]"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div variants={itemVariantsLeft}>
+        <HeroLeft /> {/* Your HeroLeft component */}
+      </motion.div>
+      <motion.div variants={itemVariantsRight}>
+        <HeroRight /> {/* Your HeroRight component */}
+      </motion.div>
+    </motion.div>
   );
 }
 
