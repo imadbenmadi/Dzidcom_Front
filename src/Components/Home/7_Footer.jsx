93d3a9a23e9a7da7d1af17182391a297aa4262dd
imadbenmadi <imadbenmadi891@gmail.com>
2024-06-02 12:37:17 +0100

    .

--- src/Components/Home/7_Footer.jsx
+++ src/Components/Home/7_Footer.jsx
@@ -18,52 +18,74 @@ function Footer() {
     visible: { opacity: 1, y: 0, transition: { duration: 1 } },
   };
   return (
-    <motion.div
-      ref={ref}
-      variants={itemVariants}
-      initial="hidden"
-      animate={isInView ? "visible" : ""}
-      className=" bg-zinc-100 py-4 mt-10 "
-    >
-      <div className="max-w-[1200px] mx-auto text-center">
-        <div>
-          <div className="flex  content-center max-md:gap-4 max-md:flex-col justify-center items-center max-w-full text-sm font-bold leading-5 text-zinc-800  max-md:flex-wrap">
-            <a href="#Hero" className="   w-fit max-md:w-fit  my-auto">
-              <div className="text-center  ">Why choose us</div>
-            </a>
-            <a href="#Goal" className="  w-[12%] max-md:w-fit  my-auto">
-              <div className="text-center">Our goals</div>
-            </a>
+      <motion.div
+          ref={ref}
+          variants={itemVariants}
+          initial="hidden"
+          animate={isInView ? "visible" : ""}
+          className=" bg-zinc-100 py-4 mt-10 "
+      >
+          <div className="max-w-[1200px] mx-auto text-center">
+              <div>
+                  <div className="flex  content-center max-md:gap-4 max-md:flex-col justify-center items-center max-w-full text-sm font-bold leading-5 text-zinc-800  max-md:flex-wrap">
+                      <a
+                          href="#Hero"
+                          className="   w-fit max-md:w-fit  my-auto"
+                      >
+                          <div className="text-center  ">Why choose us</div>
+                      </a>
+                      <a
+                          href="#Goal"
+                          className="  w-[12%] max-md:w-fit  my-auto"
+                      >
+                          <div className="text-center">Our goals</div>
+                      </a>
 
-            <a href="#Our_features" className=" w-[12%] my-auto max-md:w-fit ">
-              Our services
-            </a>
-            <Link to={"/"} className="select-none  :w-[20%] max-md:order-first">
-              <img src={Logo} alt="Logo" className=" w-full lg:w-[145px] " />
-            </Link>
-            <a href="#Contact_us" className=" w-[12%] max-md:w-fit    my-auto">
-              <div>Contact us </div>
-            </a>
-            <Link to="/Login" className="  w-[12%] ">
-              <div>Hire</div>
-            </Link>
-            <Link to="/Login" className=" w-[12%] max-md:w-fit my-auto">
-              Work
-            </Link>
+                      <a
+                          href="#Our_features"
+                          className=" w-[12%] my-auto max-md:w-fit "
+                      >
+                          Our services
+                      </a>
+                      <Link
+                          to={"/"}
+                          className="select-none  :w-[20%] max-md:order-first"
+                      >
+                          <img
+                              src={Logo}
+                              alt="Logo"
+                              className=" w-full lg:w-[145px] "
+                          />
+                      </Link>
+                      <a
+                          href="#Contact_us"
+                          className=" w-[12%] max-md:w-fit    my-auto"
+                      >
+                          <div>Contact us </div>
+                      </a>
+                      <Link to="/Login" className="  w-[12%] ">
+                          <div>Hire</div>
+                      </Link>
+                      <Link
+                          to="/Privacy"
+                          className=" w-[12%] max-md:w-fit my-auto"
+                      >
+                          Privacy
+                      </Link>
+                  </div>
+              </div>
+              <hr className="h-[2px] my-2 max-md:hidden bg-gray-300"></hr>
+              <div className="flex justify-center gap-2 items-center mx-auto  w-full  mt-12 max-md:mt-10">
+                  <img src={feacbook} className=" w-12 " />
+                  <img src={Instagram} className=" w-12 " />
+                  <img src={Linkedin} className=" w-12 " />
+                  <img src={Twitter} className=" w-12 " />
+              </div>
+              <div className="self-center mt-16 text-sm leading-5 text-center text-black max-md:mt-10">
+                  © {currentYear}
+              </div>
           </div>
-        </div>
-        <hr className="h-[2px] my-2 max-md:hidden bg-gray-300"></hr>
-        <div className="flex justify-center gap-2 items-center mx-auto  w-full  mt-12 max-md:mt-10">
-          <img src={feacbook} className=" w-12 " />
-          <img src={Instagram} className=" w-12 " />
-          <img src={Linkedin} className=" w-12 " />
-          <img src={Twitter} className=" w-12 " />
-        </div>
-        <div className="self-center mt-16 text-sm leading-5 text-center text-black max-md:mt-10">
-          © {currentYear}
-        </div>
-      </div>
-    </motion.div>
+      </motion.div>
   );
 }
 
