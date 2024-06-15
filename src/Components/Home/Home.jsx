66a2555de1c76e07fe99fb05f21c11cb29982deb
imadbenmadi <imadbenmadi891@gmail.com>
2024-06-15 15:07:35 +0100

    refactor: Update footer social media icons and add Gmail icon

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -14,8 +14,7 @@ import contact_image from "../../../public/Home/Contact/contact_image.png";
 
 import Facebook_image from "../../../public/Home/Footer/Facebook.png";
 import Instagram_image from "../../../public/Home/Footer/Instagram.png";
-import Linkedin_image from "../../../public/Home/Footer/Linkedin.png";
-import Twitter_image from "../../../public/Home/Footer/Twitter.png";
+import gmail_image from "../../../public/Home/Footer/gmail.png";
 
 import hero_arrow from "../../../public/Home/Hero/Arrow.png";
 import hero_icon from "../../../public/Home/Hero/icon.png";
@@ -43,101 +42,103 @@ import NavBar from "../../Components/Home/NavBar/NavBar";
 
 import Logo from "../../../public/Logo.png";
 function Home() {
-  const [loading, setLoading] = useState(true);
-  const [isChecked, setIsChecked] = useState(
-    localStorage.getItem("isChecked") === "true" ? true : false
-  );
-  useEffect(() => {
-    const fetch_images = () => {
-      return new Promise((resolve, reject) => {
-        const images = [
-          login_image,
-          register_image,
-          user1,
-          contact_image,
-          Facebook_image,
-          Instagram_image,
-          Linkedin_image,
-          Twitter_image,
-          hero_arrow,
-          hero_icon,
-          hero_search,
-          Hero1,
-          Hero2,
-          Hero3,
-          ower_goal_image,
-          ower_goal_star,
-          ower_goal_stars,
-          ower_goal_item,
-          Services_image1,
-          Services_image2,
-          Services_image3,
-          Why_choose_us_image1,
-          Why_choose_us_image2,
-          Why_choose_us_image3,
-          Why_choose_us_image4,
-          Why_choose_us_image5,
-          Why_choose_us_image6,
-        ];
-        let loadedCount = 0;
-        if (images.length === 0) resolve();
-        images.forEach((imageSrc) => {
-          const img = new Image();
-          img.onload = () => {
-            loadedCount++;
-            if (loadedCount === images.length) {
-              resolve(); // Resolve promise when all images are loaded
-            }
-          };
-          img.onerror = () => {
-            resolve(); // Reject if any image fails to load
-          };
-          img.src = imageSrc;
-        });
-      });
-    };
-
-    Promise.all([fetch_images()])
-      .then(() => {
-        setLoading(false);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-  }, []);
-  if (loading) {
-    return (
-      <div className=" w-screen h-screen flex flex-col items-center justify-center">
-        <img src={Logo} alt="" />
-        <span className="loader"></span>
-      </div>
+    const [loading, setLoading] = useState(true);
+    const [isChecked, setIsChecked] = useState(
+        localStorage.getItem("isChecked") === "true" ? true : false
     );
-  }
-  return (
-    <div className=" dark:bg-black dark:text-white scroll-smooth duration-500 scroll-d relative min-h-h-screen overflow-y-auto custom-overflow overflow-x-hidden ">
-      <NavBar />
-      <div className=" mt-[50px] md:mt-[60px] h-fit ">
-        <div className="scroll-smooth  ">
-          <div id="Hero" className=" max-w-[1200px] mx-auto h-fit">
-            <Hero isChecked={isChecked} setIsChecked={setIsChecked} />
-            <Why_Choose_us />
-          </div>
-          <div id="Goal">
-            <Ower_Goal isCheaked={isChecked} />
-          </div>
-          <div id="Our_features" className=" max-w-[1200px] mx-auto">
-            <Services isChecked={isChecked} />
-          </div>
+    useEffect(() => {
+        const fetch_images = () => {
+            return new Promise((resolve, reject) => {
+                const images = [
+                    login_image,
+                    register_image,
+                    user1,
+                    contact_image,
+                    Facebook_image,
+                    Instagram_image,
+                    gmail_image,
+                    hero_arrow,
+                    hero_icon,
+                    hero_search,
+                    Hero1,
+                    Hero2,
+                    Hero3,
+                    ower_goal_image,
+                    ower_goal_star,
+                    ower_goal_stars,
+                    ower_goal_item,
+                    Services_image1,
+                    Services_image2,
+                    Services_image3,
+                    Why_choose_us_image1,
+                    Why_choose_us_image2,
+                    Why_choose_us_image3,
+                    Why_choose_us_image4,
+                    Why_choose_us_image5,
+                    Why_choose_us_image6,
+                ];
+                let loadedCount = 0;
+                if (images.length === 0) resolve();
+                images.forEach((imageSrc) => {
+                    const img = new Image();
+                    img.onload = () => {
+                        loadedCount++;
+                        if (loadedCount === images.length) {
+                            resolve(); // Resolve promise when all images are loaded
+                        }
+                    };
+                    img.onerror = () => {
+                        resolve(); // Reject if any image fails to load
+                    };
+                    img.src = imageSrc;
+                });
+            });
+        };
 
-          <Clients_said isChecked={isChecked} />
-          <div id="Contact_us">
-            <Contact />
-          </div>
-          <Footer />
+        Promise.all([fetch_images()])
+            .then(() => {
+                setLoading(false);
+            })
+            .catch(() => {
+                setLoading(false);
+            });
+    }, []);
+    if (loading) {
+        return (
+            <div className=" w-screen h-screen flex flex-col items-center justify-center">
+                <img src={Logo} alt="" />
+                <span className="loader"></span>
+            </div>
+        );
+    }
+    return (
+        <div className=" dark:bg-black dark:text-white scroll-smooth duration-500 scroll-d relative min-h-h-screen overflow-y-auto custom-overflow overflow-x-hidden ">
+            <NavBar />
+            <div className=" mt-[50px] md:mt-[60px] h-fit ">
+                <div className="scroll-smooth  ">
+                    <div id="Hero" className=" max-w-[1200px] mx-auto h-fit">
+                        <Hero
+                            isChecked={isChecked}
+                            setIsChecked={setIsChecked}
+                        />
+                        <Why_Choose_us />
+                    </div>
+                    <div id="Goal">
+                        <Ower_Goal isCheaked={isChecked} />
+                    </div>
+                    <div id="Our_features" className=" max-w-[1200px] mx-auto">
+                        <Services isChecked={isChecked} />
+                    </div>
+
+                    <Clients_said isChecked={isChecked} />
+                    <div id="Contact_us">
+                        <Contact />
+                    </div>
+                    <Footer />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
